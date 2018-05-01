eace59f5665d3d05af0dc3cb8ae1c05bb168b3c0
augustoaraujo97 <augustoaraujo.97@gmail.com>
2018-05-01 14:40:45 -0300

    cliente

--- Carteira-mvn/src/main/java/Cliente/ClienteResource.java
+++ Carteira-mvn/src/main/java/Cliente/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -30,11 +31,31 @@ public class ClienteResource {
     //@Path("/inserir")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response postCliente(){
+    public Response postCliente(String content) throws Exception{
         try{
             ClienteDao dao = DaoFactory.getClienteDao();
-            List<Cliente> cliente = dao.findAll();
-            return Response.ok(new Clientes(cliente)).build();
+   
+            boolean ok  = dao.insert(content); 
+            
+            return Response.ok().build();
+                    
+        } catch(DaoException ex){
+            return Response.serverError().entity(ex.getMessage()).build();
+        }
+    }
+    
+    @PUT
+    //@Path("/inserir")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response putCliente(String content) throws Exception{
+        try{
+            ClienteDao dao = DaoFactory.getClienteDao();
+   
+            boolean ok  = dao.update(content); 
+            
+            return Response.ok().build();
+                    
         } catch(DaoException ex){
             return Response.serverError().entity(ex.getMessage()).build();
         }
